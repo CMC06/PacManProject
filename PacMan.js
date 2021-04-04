b8a69ae36ea1f5aabfdab407d6fbcd699ce1f360
CMC06 <crzychelc@gmail.com>
2021-04-03 20:44:51 -0400

    added logic for PacMan to eat fruit and fruit to spawn when one fruit is eaten

--- PacMan.js
+++ PacMan.js
@@ -11,6 +11,27 @@ let direction = 0;
 let focus = 0;
 let score = 0;
 
+function fruitAppear(){
+  const fruitDiv = document.getElementById("fruitDiv");
+  let currentFruit = fruits[Math.floor(Math.random()*fruits.length)];
+  let currentX = Math.floor(Math.random()* (window.innerWidth - 50));
+  let currentY = Math.floor(Math.random()* (window.innerHeight - 50));
+  
+  let fruitImage = document.createElement('img');
+  fruitImage.id = 'fruitImage';
+  fruitImage.src = currentFruit;
+  
+  fruitDiv.appendChild(fruitImage);
+
+  fruitImage.style.position = 'absolute';
+  fruitImage.style.top = currentY + 'px';
+  fruitImage.style.left = currentX + 'px';
+  fruitImage.style.width = "50px";
+  fruitImage.style.zIndex = 0;
+}
+fruitAppear();  
+//setInterval(fruitAppear, 4000);
+
 function mouth() {
   let img = document.getElementById("PacMan");
   focus = (focus + 1) % 2;
@@ -74,7 +95,7 @@ window.addEventListener("keydown", function(event){
   event.preventDefault();
 }, true);
 
-
+//add the gobble() call as conditional based on position coordinates
 function moveDown() {
   let img = document.getElementById("PacMan")
   posY += 20;
@@ -97,6 +118,7 @@ function moveUp() {
 
 function moveLeft() {
   let img = document.getElementById("PacMan");
+  let fruit = document.getElementById("fruitImage");
   posX -= 20;
   if(posX < 0){
     posX = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -104,10 +126,14 @@ function moveLeft() {
   direction = 1;
   img.style.left = posX + "px";
   mouth();
+  if(Math.abs(parseInt(img.style.left) - parseInt(fruit.style.left) + 25) <= 10 && Math.abs(parseInt(img.style.top) - parseInt(fruit.style.top)) <= 10) {
+    gobble();
+  }
 }
 
 function moveRight() {
   let img = document.getElementById("PacMan");
+  let fruit = document.getElementById("fruitImage");
   posX += 20;
   if(posX + img.width >= Math.max(document.documentElement.clientWidth, window.innerWidth || 0)){
     posX = 0;
@@ -115,26 +141,13 @@ function moveRight() {
   direction = 0;
   img.style.left = posX + "px";
   mouth();
+  if(Math.abs(parseInt(img.style.left) - parseInt(fruit.style.left) - 25) <= 10 && Math.abs(parseInt(img.style.top) - parseInt(fruit.style.top)) <= 10) {
+    gobble();
+  }
 }
 
-
-
-function fruitAppear(){
-  const fruitDiv = document.getElementById("fruitDiv");
-  let currentFruit = fruits[Math.floor(Math.random()*fruits.length)];
-  let currentX = Math.floor(Math.random()* (window.innerWidth - 50));
-  let currentY = Math.floor(Math.random()* (window.innerHeight - 50));
-  
-  let fruitImage = document.createElement('img');
-  fruitImage.src = currentFruit;
-  
-  fruitDiv.appendChild(fruitImage);
-
-  fruitImage.style.position = 'absolute';
-  fruitImage.style.top = currentY + 'px';
-  fruitImage.style.left = currentX + 'px';
-  fruitImage.style.width = "50px";
-  fruitImage.style.zIndex = 0;
+function gobble(){
+  let fruit = document.getElementById("fruitImage");
+  fruit.remove();
+  fruitAppear();
 }
-fruitAppear();  
-//setInterval(fruitAppear, 4000);
